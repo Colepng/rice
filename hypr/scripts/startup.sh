1252ebe05251fa890087409f450f223a6047173f
Colepng <gamingwithcole7@gmail.com>
2023-02-19 10:41:32 -0500

    Update hyprland config

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #Apps
+rfkill block bluetooth &
 dunst &
 discord --start-minimized &
-eww open bar0
-sleep 1 &&
 swww init &
-sleep 1 &&
-swww img /home/cole/Pictures/night.jpg
+wpg -m &
+waybar &
+udiskie &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+mpd-discord-rpc & 
