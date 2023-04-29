6c524462ce0340bced474344b0f8cafeaa0325b1
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-04-29 13:53:26 -0400

    Update nvim

--- nvim/after/plugin/colours.lua
+++ nvim/after/plugin/colours.lua
@@ -1,9 +1,9 @@
 function ColourMyPenciles(colour)
-    colour = colour -- or "vscode"
-	vim.cmd.colorscheme(colour)
-    vim.keymap.set('n', '<leader>cs', vim.cmd.colorscheme("pywal"), {})
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- colour = colour or "vscode"
+	vim.cmd.colorscheme("vscode")
+    -- vim.keymap.set('n', '<leader>cs', vim.cmd.colorscheme("pywal"), {})
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "#181921" })
+	-- vim.api.nvim_set_hl(1, "NormalFloat", { bg = "#181921" })
 end
 
 ColourMyPenciles(pywal)
